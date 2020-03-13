db760c74fd3922aaf37550d7eadd4062cbe34532
M Nef <howtoeatpineapples@gmail.com>
2020-03-14 02:47:57 +0800

    simpler paging system

--- gym_pagedbandit/bandit.py
+++ gym_pagedbandit/bandit.py
@@ -13,23 +13,20 @@ class PagesdBanditEnv(gym.Env):
         A list of either rewards (if number) or means and standard deviations (if list)
         of the payout that bandit has
     """
-    def __init__(self, p_dist, r_dist, pagesize):
+    def __init__(self, p_dist, r_dist):
         if len(p_dist) != len(r_dist):
             raise ValueError("Probability and Reward distribution must be the same length")
 
         if min(p_dist) < 0 or max(p_dist) > 1:
             raise ValueError("All probabilities must be between 0 and 1")
 
-        if pagesize <= 0:
-            raise ValueError("pagesize must be at least 1")
-
         for reward in r_dist:
             if isinstance(reward, list) and reward[1] <= 0:
                 raise ValueError("Standard deviation in rewards must all be greater than 0")
 
-        self.pagesize = pagesize
         self.page_no = 0
-        self.page_count = int(np.math.ceil(len(r_dist) / pagesize)) 
+        self.pages = [[0, 1, 2, 3 , 4], [5, 6, 7, 8, 9]]
+        self.current_page = self.pages[self.page_no]
 
         self.p_dist = p_dist
         self.r_dist = r_dist
@@ -44,28 +41,31 @@ class PagesdBanditEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def setPage(self, pageNo):
-        ...
-
     def step(self, action):
         assert self.action_space.contains(action)
-        
+    
         if action == self.pagesize + 1:
+            self.page_no = (self.page_no + 1) % len(self.pages)
+            self.current_page = self.pages[self.page_no]
 
+            reward = 0
+            return self.current_page, reward, False, {'page_no': self.page_no}
+        else:
+            reward = 0
+            done = True
 
-        reward = 0
-        done = True
+            if np.random.uniform() < self.p_dist[action]:
+                if not isinstance(self.r_dist[action], list):
+                    reward = self.r_dist[action]
+                else:
+                    reward = np.random.normal(self.r_dist[action][0], self.r_dist[action][1])
 
-        if np.random.uniform() < self.p_dist[action]:
-            if not isinstance(self.r_dist[action], list):
-                reward = self.r_dist[action]
-            else:
-                reward = np.random.normal(self.r_dist[action][0], self.r_dist[action][1])
-
-        return 0, reward, done, {}
+            return self.current_page, reward, done, {'page_no': self.page_no}
 
     def reset(self):
-        return 0
+        self.page_no = 0
+        self.current_page = self.pages[self.page_no]
+        return self.current_page
 
     def render(self, mode='human', close=False):
         pass
