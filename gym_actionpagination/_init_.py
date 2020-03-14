2855032b5b0143decc224ff937a9825022a6b21c
M Nef <howtoeatpineapples@gmail.com>
2020-03-14 11:06:22 +0800

    updated some package data

--- gym_actionpagination/_init_.py
+++ gym_actionpagination/_init_.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id=f'actionpagination-v0',
-    entry_point=f'gym_actionpagination.envs:PagedBanditEnv'
+    id='actionpagination-v0',
+    entry_point='gym_actionpagination.envs:PagedBanditEnv'
 )
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='gym_pagedbandit',
+setup(name='gym_actionpagination',
       version='0.0.1',
       description='Bandits environment with action paging',
       url='https://github.com/MouseAndKeyboard/gym-actionpagination',
       packages=['gym_actionpagination'],
       author_email='michael@nef.net.au',
       license='GPL-3.0',
-      install_requires=['gym', 'gym_bandits'],
+      install_requires=['gym'],
 )
